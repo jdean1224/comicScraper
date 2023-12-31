026254c15780a0e568ada91b3395b9cf1ed58492
Justin Koski <jdeankoski@gmail.com>
2023-12-31 17:15:48 -0500

    new config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This should match your "publish" directory in Netlify
+  },
+});
